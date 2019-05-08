5b6f06a1df274059a626af55f97531d253661e5a
Warayut Taekrathok <warayut2555@gmail.com>
2019-05-08 11:36:16 +0700

    50%

---
+++ VIVOSHOP/Controllers/BankController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VIVOSHOP.Models;
+
+namespace VIVOSHOP.Controllers
+{
+    public class BankController : Controller
+    {
+        public vivoshopDBEntities db = new vivoshopDBEntities();
+        // GET: Bank
+        public ActionResult Index(string keyword)
+        {
+            if (keyword == " " || keyword == null)
+            {
+                return View(db.Banks.ToList());
+            }
+            else
+            {
+                var c = db.Banks.Where(x => x.Bank_Name.Contains(keyword)).ToList();
+                var b = db.Banks.Where(x => x.Bank_Number.Contains(keyword)).ToList();
+                var A = db.Banks.Where(x => x.Bank_Number.Contains("123456789")).ToList();
+                if (b.Count > 0)
+                {
+                    A = b;
+                }
+                if (c.Count > 0)
+                {
+                    A = c;
+                }
+                return View(A);
+            } 
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Bank_Number,Bank_Name,Bank_User")] Bank bank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Banks.Add(bank);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            return View(bank);
+        }
+
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Bank_Number,Bank_Name,Bank_User")] Bank bank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bank).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bank);
+        }
+         
+
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bank bank = db.Banks.Find(id);
+            if (bank == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bank bank = db.Banks.Find(id);
+            if (bank==null)
+            {
+                return HttpNotFound();
+            }
+            return View(bank); 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VIVOSHOP/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VIVOSHOP.Models;
+
+namespace VIVOSHOP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public vivoshopDBEntities db = new vivoshopDBEntities();
+        public ActionResult Index(string keyword)
+        {
+            if (keyword == " " || keyword == null)
+            {
+                return View(db.Products.ToList());
+            }
+            else
+            {
+                return View(db.Products.Where(x => x.Pro_Name.Contains(keyword)).ToList());
+            }
+
+        }  
+
+        public ActionResult Create(string id,string amout)
+        {
+            List<string> modes = new List<string>();
+            modes.Add("Simple");
+            modes.Add("Advanced");
+            modes.Add("Manual");
+            modes.Add("Complete");
+            ViewBag["Modes"] = modes;
+            return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        } 
+    }
+}
